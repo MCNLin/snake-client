5238d57a391b12d13265c7be9fd13c21e1d40889
Mei Nghiem-Lin <mcnghiem@gmail.com>
2021-11-03 19:35:13 +0000

    add connect event to include name intials

--- client.js
+++ client.js
@@ -2,4 +2,10 @@ const net = require('net');
 
 const {connect} = require('./play');
 
-
+const conn = net.createConnection({
+  host: '165.227.47.243',
+  port: 50541
+});
+conn.on('connect', () => {
+  conn.write("Name: MCN");
+});
