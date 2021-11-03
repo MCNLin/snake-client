0ad7efaa264c0d89f9345ed5bb76a085dc844055
Mei Nghiem-Lin <mcnghiem@gmail.com>
2021-11-03 22:22:11 +0000

    comment out hard code

--- client.js
+++ client.js
@@ -7,5 +7,25 @@ const conn = net.createConnection({
   port: 50541
 });
 conn.on('connect', () => {
-  conn.write("Name: MCN");
+  conn.write("Name: MCN");   
+});
+conn.on('connect', () => {
+  conn.write('will this work?');   
+});
+conn.on('connect', () => {
+  conn.write("hellllloo");   
+});
+conn.on('connect', () => {
+//   setInterval(() => {conn.write('Move: up')}, 50);
+//   setInterval(() => {conn.write('Move: up')}, 100);
+//   setInterval(() => {conn.write('Move: left')}, 150);
+//   setInterval(() => {conn.write('Move: left')}, 200);
+//   setInterval(() => {conn.write('Move: left')}, 250);
+//   setInterval(() => {conn.write('Move: up')}, 300);
+//   setInterval(() => {conn.write('Move: up')}, 350);
+//   setInterval(() => {conn.write('Move: up')}, 400);
+//   setInterval(() => {conn.write('Move: right')}, 450);
+//   setInterval(() => {conn.write('Move: right')}, 500);
+//   setInterval(() => {conn.write('Move: right')}, 550);
+//   setInterval(() => {conn.write('Move: right')}, 600);
 });
